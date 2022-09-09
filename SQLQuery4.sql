22367d6355781660ebdf712f10a7ec231900770f
Lperezn <111660067+Lperezn@users.noreply.github.com>
2022-09-08 22:56:49 -0500

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,29 @@
+create database Hospitalizacion
+use Hospitalizacion
+
+create table Medicos(
+IdMedico int primary key identity(1,1) not null,
+Nombre varchar (40) not null,
+Apellido varchar (40) not null,
+NumeroTelefonico varchar (40) not null,
+Genero varchar(40) not null,
+Especialidad varchar (40),
+Codigo int,
+Registro_Rethus int,
+)
+
+create table Pacient(
+IdPaciente int primary key identity(1,1) not null,
+IdMedico int,
+Nombre varchar (40) not null,
+Apellido varchar (40) not null,
+NumeroTelefonico varchar (40) not null,
+Genero varchar(40) not null,
+Direccion int,
+Ciudad int,
+FechaIngreso date,
+Habitacion int,
+
+CONSTRAINT fk_Medicos FOREIGN KEY (IdMedico) REFERENCES Medicos(IdMedico)
+
+);
